e815a4ba99a745f0cf132ec3376bc65bb9c59ed6
John Kostanov <kostanovd@icloud.com>
2021-03-17 16:52:08 +0100

    Initial Commit

---
+++ TestNewForBitbucket/TestNewForBitbucketApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewForBitbucketApp.swift
+//  TestNewForBitbucket
+//
+//  Created by Джон Костанов on 17/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TestNewForBitbucketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
